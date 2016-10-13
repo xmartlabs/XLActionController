f063cd7fdb7b11b1a2b50155bf72ade169fc20f6
Miguel Revetria <miguel@xmartlabs.com>
2016-10-13 08:09:37 -0300

    Fixed #30, removed missing outlets

--- Source/ActionCell.swift
+++ Source/ActionCell.swift
@@ -36,10 +36,24 @@ open class ActionCell: UICollectionViewCell, SeparatorCellType {
     @IBOutlet open weak var actionDetailLabel: UILabel?
     @IBOutlet open weak var separatorView: UIView?
 
+    @IBOutlet open weak var imageViewWidthConstraint: NSLayoutConstraint?
+
+    var imageWidth: CGFloat = 0
+
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+
+        imageWidth = imageViewWidthConstraint?.constant ?? 0
+    }
+
     open func setup(_ title: String?, detail: String?, image: UIImage?) {
         actionTitleLabel?.text = title
         actionDetailLabel?.text = detail
         actionImageView?.image = image
+
+        imageViewWidthConstraint?.constant = image == nil ? 0 : imageWidth
+
+        setNeedsLayout()
     }
     
     open func showSeparator() {
