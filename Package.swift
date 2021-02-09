3dc636f7fe45c1dd27c3c3969b22c8e9ef6ecaec
Mathias Claassen <mathias@xmartlabs.com>
2021-02-09 09:47:15 -0300

    Add package supspecs

--- Package.swift
+++ Package.swift
@@ -2,18 +2,27 @@
 
 import PackageDescription
 
+func getExampleTarget(name: String) -> Target {
+    return .target(name: "XLActionController_" + name,
+                   path: "Example/CustomActionControllers",
+                   dependencies: [Target.Dependency.target(name: "XLActionController")],
+                   sources: ["\(name).swift", "ActionData.swift"],
+                   resources: ["\(name).xib"])
+}
+
 let package = Package(
     name: "XLActionController",
         platforms: [
         .iOS(.v9)
     ],
     products: [
-        .library(name: "XLActionController", targets: ["XLActionController"])
+        .library(name: "XLActionController", targets: ["XLActionController"]),
+        .library(name: "XLActionController", targets: ["XLActionController_Skype"]),
+        .library(name: "XLActionController", targets: ["XLActionController_Spotify"])
     ],
     targets: [
-        .target(
-            name: "XLActionController",
-            path: "Source"
-        )
+        .target(name: "XLActionController", path: "Source"),
+        getExampleTarget(name: "Skype"),
+        getExampleTarget(name: "Spotify"),        
     ]
 )
